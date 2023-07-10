b3f121b9904cfe88ca7a69a32b1a2f3889df8711
Aditya Singh <96740553+AdityaSingh21074@users.noreply.github.com>
2023-07-10 18:47:15 +0530

    Small java projects to learn developing in java

--- src/com/practiceMyClock/Main.java
+++ src/com/practiceMyClock/Main.java
@@ -1,8 +1,8 @@
-package com.practice;
+package com.practiceMyClock;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("testing...");
+        System.out.print("clock running...");
         MyWindow w=new MyWindow();
     }
 }
--- src/com/practiceMyClock/MyWindow.java
+++ src/com/practiceMyClock/MyWindow.java
@@ -1,4 +1,4 @@
-package com.practice;
+package com.practiceMyClock;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
